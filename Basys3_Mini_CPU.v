9b37d1b673718a1ee52d4be984c77a9d681661ae
yuechuan <xueyuechuan@gmail.com>
2015-07-15 15:07:35 +0800

    [bug fix] Reg_File parameter

--- Basys3_Mini_CPU.v
+++ Basys3_Mini_CPU.v
@@ -19,13 +19,54 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module Basys3_Mini_CPU(
 
     );
     
+    reg        rst;
+    reg        clk;
+    reg  [3:0] porta_in;
+    reg  [7:0] portb_in;
+    wire [3:0] porta_out;
+    wire [7:0] portb_out;
+    wire [3:0] porta_tris;
+    wire [7:0] portb_tris;
+
+    reg        wdtmr;
+    reg        tmr0_inc;
+    wire [7:0] option_out;
+ 
+ 	PIC16F54   PIC16F54_01(
+    .rst(rst),
+    .clk(clk),
+    .porta_in(porta_in),
+    .portb_in(portb_in),
+    .porta_out(porta_out),
+    .portb_out(portb_out), 
+    .porta_tris(porta_tris),
+    .portb_tris(portb_tris),
+    .wdtmr(wdtmr),
+    .tmr0_inc(tmr0_inc),
+    .option_out(option_out)
+    );
     
-     PIC16F54       PIC16F54_tb01(
-   
-     );
+ 	always #(tp/2) clk = ~clk;  // generating clk
+    initial #(30000*tp) $stop;
+
+    initial begin
+    	rst = 1;
+    	clk = 0;
+    	porta_in = 4'b0000;
+    	portb_in = 8'b00000000;
+    	wdtmr = 0;
+    	tmr0_inc = 0;
+
+    	# 100
+    	
+    end
+    
+    
+
+
 endmodule
+
